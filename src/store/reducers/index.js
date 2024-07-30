f9f71872d6bb42603f513e344cbfcf17f4b5717c
lucerion <alexander.skachko@gmail.com>
2024-07-30 04:07:52 +0300

    Fix fields deletion

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,26 +1,29 @@
 import { ACTION_TYPES } from '../actions';
 
-const reducers = (state, action) => {
-  const { addField, deleteField, changeField } = ACTION_TYPES;
-  const { fields } = state;
-  const { field, type } = action;
+const { addField, deleteField, changeField } = ACTION_TYPES;
 
+const newFields = ({ fields }, { field, type }) => {
   switch (type) {
   case addField: {
     const lastID = Object.keys(fields).at(-1);
     const id = lastID ? parseInt(lastID) + 1 : 0;
 
-    return { ...state, fields: { ...fields, [id]: { ...field, id } } };
+    return { ...fields, [id]: { id, ...field } };
   }
   case deleteField: {
-    return { ...state, fields: Reflect.deleteProperty({ ...fields }, field.id) };
+    const newFields = { ...fields };
+    delete newFields[field.id];
+
+    return newFields;
   }
   case changeField: {
-    return { ...state, fields: { ...fields, [field.id]: field } };
+    return { ...fields, [field.id]: field };
   }
   default:
     throw new Error(`'${type}' not found!`);
   }
 };
+
+const reducers = (state, action) => ({ ...state, fields: newFields(state, action) });
 
 export default reducers;
